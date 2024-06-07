2067d77afa6b0c845f5c5bd9628320a67843dc0f
Myonya <rahimovbogdan45@gmail.com>
2024-06-07 13:59:46 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} сейчас в сети.`);   
+})
+
+client.on('messageCreate', (message) => {
+        //console.log(message);
+        console.log(message.author.username + ': ' + message.content);
+    if (message.author.bot) {
+        return;
+    }
+    if (message.author.username === 'muerv') {
+        const replies = ['Молчать, женщина', 'Тише-тише, терпила', 'Заткнись, пупуня', 'Как же ты меня заебёшь', 'Ротик'];
+        const randomReply = replies[Math.floor(Math.random() * replies.length)];
+        message.reply(randomReply);
+    }   
+    if (message.content === 'Полина ебень' || message.content === 'полина ебень' || message.content === 'я ебень') {
+        const replies = ['Согласен', 'Абсолютно верно', 'Без сомнений'];
+        const randomReply = replies[Math.floor(Math.random() * replies.length)];
+        message.reply(randomReply);
+    }
+    if (message.content === 'Полина пупуня'　|| message.content === 'полина пупуня' || message.content === 'я пупуня') {
+        message.reply('https://tenor.com/view/nyatasha-nyanners-nyanners-vtuber-like-a-dragon-ishin-based-gif-10588927962711773317');
+    }
+    if (message.content === 'Дурень' || message.content === 'дурень') {
+        message.reply('Ебало');
+    }    
+});
+
+client.login(process.env.TOKEN);
